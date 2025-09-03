1676dca35e21c90d4a299db1c33cdce2e8d07b96
essie-kamau043 <156332730+essie-kamau043@users.noreply.github.com>
2025-09-03 17:28:17 +0300

    Add README.md and rust-hello-world folder

---
+++ target/debug/deps/rust_hello_world-b374334e57712cbf.d
@@ -0,0 +1,5 @@
+/home/missy/rust-hello-world/target/debug/deps/rust_hello_world-b374334e57712cbf.d: src/main.rs
+
+/home/missy/rust-hello-world/target/debug/deps/rust_hello_world-b374334e57712cbf: src/main.rs
+
+src/main.rs:
---
+++ target/debug/rust-hello-world.d
@@ -0,0 +1,1 @@
+/home/missy/rust-hello-world/target/debug/rust-hello-world: /home/missy/rust-hello-world/src/main.rs
